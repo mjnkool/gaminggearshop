3c9a5d35319a1ec5189feeb47878f270364427b9
mjnkool <hoangdinhthangloi@gmail.com>
2020-05-28 17:13:37 +0700

    5:14 28/5/2020 at Kem-PC

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('admin_layout')
+@section('admin_contain')
+<h3>Chào mừng bạn đến với quản trị Admin</h3>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Frontend
+Route::get('/', 'HomeController@index' );
+Route::get('/trang-chu','HomeController@index' );
+
+// Backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
+
+// Category Product
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+Route::post('/save-category-product','CategoryProduct@save_category_product');
+
